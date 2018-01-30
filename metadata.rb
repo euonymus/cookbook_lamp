7d4de9512e815fb8685d2934397def69fd4ffc6b
euonymus <euonymus0220@gmail.com>
2018-01-30 17:54:23 +0900

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'lamp'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures lamp'
+long_description 'Installs/Configures lamp'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: lamp
+# Recipe:: default
+#
+# Copyright (C) 2018 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
