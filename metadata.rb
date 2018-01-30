1a29233410e511950a50e9c733883361d220c8fd
euonymus <euonymus0220@gmail.com>
2018-01-30 18:05:20 +0900

    init

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,14 @@
 name             'lamp'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'euonymus'
+maintainer_email 'euonymus0220@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures lamp'
 long_description 'Installs/Configures lamp'
 version          '0.1.0'
+
+
+depends 'apache2'
+depends 'mysql', '~> 6.0'
+depends 'php'
+depends 'database'
+depends 'mysql2_chef_gem'
